84cd6cf3c3416ba846a72ffa1b3ed325ea14b367
goto2014 <mip-goto@web.de>
2015-08-05 21:24:42 +0200

    bittrex api

--- python3/pubBittrex.py
+++ python3/pubBittrex.py
@@ -3,18 +3,11 @@ import requests
 
 class pubBittrex:
 	def __init__(self):
-		self.url = "https://bittrex.com/api/v1.1/public/getorderbook?market=BTC-"
-		self.urlEnd = "&type=both&depth=50"
-
 		self.coin = "limx"
-
-
 		self.summeryURL = "https://bittrex.com/api/v1.1/public/getmarketsummary?market=btc-"
 
 	def getSummery(self):
 		
-		data = {}
-
 		path = self.summeryURL + self.coin
 
 		newData = requests.get(path).json()
