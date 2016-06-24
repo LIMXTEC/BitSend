006a7ec0e98b86b6f744898557ea2356d7a606f4
Christian Knöpke <apfelbaum@email.de>
2016-06-24 10:04:14 +0200

    X17
    Fix

--- src/main.cpp
+++ src/main.cpp
@@ -1709,7 +1709,9 @@ bool CheckProofOfWork(uint256 hash, unsigned int nBits)
         return error("CheckProofOfWork() : nBits below minimum work");
 
     // Check proof of work matches claimed amount
-    if (hash > bnTarget.getuint256())
+	// Fix for X17 
+	// old if (hash > bnTarget.getuint256())
+    if (hash > bnTarget.getuint256() && hash != hashGenesisBlock)
         return error("CheckProofOfWork() : hash doesn't match nBits");
 
     return true;
