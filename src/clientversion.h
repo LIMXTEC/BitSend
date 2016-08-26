92e746ebb2bb447edcedd60f0e439318238f704b
Christian Knöpke <apfelbaum@email.de>
2016-08-26 17:47:49 +0200

    Update clientversion.h

--- src/clientversion.h
+++ src/clientversion.h
@@ -12,7 +12,7 @@
 #define CLIENT_VERSION_MAJOR       1
 #define CLIENT_VERSION_MINOR       1
 #define CLIENT_VERSION_REVISION    0
-#define CLIENT_VERSION_BUILD       1
+#define CLIENT_VERSION_BUILD       2
 // bsdfork darkcoin 11.2.17
 
 
