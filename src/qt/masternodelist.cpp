11fca9ff0d3bb18459f2453cca256d59e900218d
joshafest <joshaf.est@gmail.com>
2017-09-09 18:11:29 +0530

    bug fix in #432a406

--- src/qt/masternodelist.cpp
+++ src/qt/masternodelist.cpp
@@ -131,11 +131,12 @@ void MasternodeList::StartAll(std::string strCommand)
         std::string strError;
         //CMasternodeBroadcast mnb;
 
-        int nIndex;
-        //if(!mne.castOutputIndex(nIndex))
-            //continue;
+        int32_t nOutputIndex = 0;
+        if(!ParseInt32(mne.getOutputIndex(), &nOutputIndex)) {
+            continue;
+        }
 
-        CTxIn txin = CTxIn(uint256S(mne.getTxHash()), uint32_t(nIndex));
+        CTxIn txin = CTxIn(uint256S(mne.getTxHash()), nOutputIndex);
         CMasternode* pmn = mnodeman.Find(txin);
 
         if (strCommand == "start-missing" && pmn) continue;
@@ -222,15 +223,16 @@ void MasternodeList::updateMyNodeList(bool fForce)
 
     ui->tableWidgetMasternodes->setSortingEnabled(false);
     BOOST_FOREACH (CMasternodeConfig::CMasternodeEntry mne, masternodeConfig.getEntries()) {
-        int nIndex;
-        //if(!mne.castOutputIndex(nIndex))
-            //continue;
-
-        CTxIn txin = CTxIn(uint256S(mne.getTxHash()), uint32_t(nIndex));
+        int32_t nOutputIndex = 0;
+        if(!ParseInt32(mne.getOutputIndex(), &nOutputIndex)) {
+            continue;
+        }
+        CTxIn txin = CTxIn(uint256S(mne.getTxHash()), nOutputIndex);
         CMasternode* pmn = mnodeman.Find(txin);
-
-        updateMyMasternodeInfo(QString::fromStdString(mne.getAlias()), QString::fromStdString(mne.getIp()), pmn);
-    }
+		
+		if(pmn != NULL){
+        updateMyMasternodeInfo(QString::fromStdString(mne.getAlias()), QString::fromStdString(mne.getIp()), pmn);}
+    } 
     ui->tableWidgetMasternodes->setSortingEnabled(true);
 
     // reset "timer"
@@ -346,6 +348,7 @@ void MasternodeList::on_startAllButton_clicked()
     // Display message box
     QMessageBox::StandardButton retval = QMessageBox::question(this, tr("Confirm all masternodes start"),
         tr("Are you sure you want to start ALL masternodes?"),
+		tr("Please make sure that wallet is unlocked"),
         QMessageBox::Yes | QMessageBox::Cancel,
         QMessageBox::Cancel);
 
