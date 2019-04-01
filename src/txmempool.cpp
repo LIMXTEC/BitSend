470a12bf32a8173ce02eaf4ae840fab3f87e3136
David <david.bergen@gmx.net>
2019-04-01 11:27:46 +0200

    fix

--- src/txmempool.cpp
+++ src/txmempool.cpp
@@ -462,7 +462,8 @@ void CTxMemPool::addAddressIndex(const CTxMemPoolEntry &entry, const CCoinsViewC
     for (unsigned int j = 0; j < tx.vin.size(); j++) {
         const CTxIn input = tx.vin[j];
         //const CTxOut &prevout = view.AccessCoins(input.prevout).out;
-        const COutPoint &prevout = tx.vin[i].prevout;
+        //const COutPoint &prevout = tx.vin[i].prevout;
+        const CTxOut &prevout = view.GetOutputFor(tx.vin[j]);
         if (prevout.scriptPubKey.IsPayToScriptHash()) {
             std::vector<unsigned char> hashBytes(prevout.scriptPubKey.begin()+2, prevout.scriptPubKey.begin()+22);
             CMempoolAddressDeltaKey key(2, uint160(hashBytes), txhash, j, 1);
@@ -538,7 +539,8 @@ void CTxMemPool::addSpentIndex(const CTxMemPoolEntry &entry, const CCoinsViewCac
     for (unsigned int j = 0; j < tx.vin.size(); j++) {
         const CTxIn input = tx.vin[j];
         //const CTxOut &prevout = view.AccessCoins(input.prevout).out;
-        const COutPoint &prevout = tx.vin[i].prevout;
+        //const COutPoint &prevout = tx.vin[i].prevout;
+        const CTxOut &prevout = view.GetOutputFor(tx.vin[j]);
         uint160 addressHash;
         int addressType;
 
