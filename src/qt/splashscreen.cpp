24256aa8f28f0314dd41121dd56b89f042790603
limxdev <limx_dev@pm.me>
2020-07-21 16:02:03 +0200

    Update splashscreen.cpp

--- src/qt/splashscreen.cpp
+++ src/qt/splashscreen.cpp
@@ -42,6 +42,8 @@ SplashScreen::SplashScreen(interfaces::Node& node, Qt::WindowFlags f, const Netw
     // define text to place
     QString titleText       = tr(PACKAGE_NAME);
     QString versionText     = QString("Version %1").arg(QString::fromStdString(FormatFullVersion()));
+    versionText = versionText.rightJustified(25, '.', true);
+    QString copyrightText   = "Welcome to Bitsend Core 0.17+\n\nOfficial website\nbitsend.cc";
     QString copyrightText   = QString::fromUtf8(CopyrightHolders(strprintf("\xc2\xA9 %u-%u ", 2009, COPYRIGHT_YEAR)).c_str());
     QString titleAddText    = networkStyle->getTitleAddText();
 
