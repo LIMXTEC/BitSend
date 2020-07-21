79f9e9be56a912b7bae182f7a1bd7c76c40c1d88
limxdev <limx_dev@pm.me>
2020-07-21 15:58:05 +0200

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -132,13 +132,10 @@ public:
         // This is fine at runtime as we'll fall back to using them as a oneshot if they don't support the
         // service bits we want, but we should get them updated to support all service bits wanted by any
         // release ASAP to avoid it where possible.
-        vSeeds.emplace_back("seed.bitsend.sipa.be"); // Pieter Wuille, only supports x1, x5, x9, and xd
-        vSeeds.emplace_back("dnsseed.bluematt.me"); // Matt Corallo, only supports x9
-        vSeeds.emplace_back("dnsseed.bitsend.dashjr.org"); // Luke Dashjr
-        vSeeds.emplace_back("seed.bitsendstats.com"); // Christian Decker, supports x1 - xf
-        vSeeds.emplace_back("seed.bitsend.jonasschnelli.ch"); // Jonas Schnelli, only supports x1, x5, x9, and xd
-        vSeeds.emplace_back("seed.btc.petertodd.org"); // Peter Todd, only supports x1, x5, x9, and xd
-        vSeeds.emplace_back("seed.bitsend.sprovoost.nl"); // Sjors Provoost
+        vSeeds.emplace_back("37.120.186.85"); // Pieter Wuille, only supports x1, x5, x9, and xd
+        vSeeds.emplace_back("85.146.167.185"); // Matt Corallo, only supports x9
+        vSeeds.emplace_back("185.194.140.60"); // Luke Dashjr
+        vSeeds.emplace_back("188.68.39.1"); // Christian Decker, supports x1 - xf
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,102);
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,5);
@@ -146,7 +143,7 @@ public:
         base58Prefixes[EXT_PUBLIC_KEY] = {0x04, 0x88, 0xB2, 0x1E};
         base58Prefixes[EXT_SECRET_KEY] = {0x04, 0x88, 0xAD, 0xE4};
 
-        bech32_hrp = "bc";
+        bech32_hrp = "bsd";
 
         vFixedSeeds = std::vector<SeedSpec6>(pnSeed6_main, pnSeed6_main + ARRAYLEN(pnSeed6_main));
 
