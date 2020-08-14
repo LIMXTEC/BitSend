5f05758435260e15a1231d8ec2127c3a6cc82032
limxdev <limx_dev@pm.me>
2020-08-14 12:50:10 +0200

    add lockwallet to walletview

--- src/qt/walletview.h
+++ src/qt/walletview.h
@@ -103,6 +103,8 @@ public Q_SLOTS:
     void changePassphrase();
     /** Ask for passphrase to unlock wallet temporarily */
     void unlockWallet();
+        /** Lock wallet */
+    void lockWallet();
 
     /** Show used sending addresses */
     void usedSendingAddresses();
