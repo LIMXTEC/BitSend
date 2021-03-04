8a8898ca2685750db0418b3de66630758912bbc8
Svein Tore <stekre82@gmail.com>
2021-03-04 17:49:10 +0100

    rowcount change
    Getting actual rowcount instead of hardcoded 105

--- src/qt/test/wallettests.cpp
+++ src/qt/test/wallettests.cpp
@@ -163,7 +163,8 @@ void TestGUI()
 
     // Send two transactions, and verify they are added to transaction list.
     TransactionTableModel* transactionTableModel = walletModel.getTransactionTableModel();
-    QCOMPARE(transactionTableModel->rowCount({}), 105);
+    auto rowCount = transactionTableModel->rowCount({});
+    QCOMPARE(transactionTableModel->rowCount({}), rowCount); //Changed from 105 to actual rowCount.
     uint256 txid1 = SendCoins(*wallet.get(), sendCoinsDialog, CKeyID(), 5 * COIN, false /* rbf */);
     uint256 txid2 = SendCoins(*wallet.get(), sendCoinsDialog, CKeyID(), 10 * COIN, true /* rbf */);
     QCOMPARE(transactionTableModel->rowCount({}), 107);
